2091af8b7842c0aefef9d134b3626389a554a2b1
Darioromaobianchi <89203731+Darioromaobianchi@users.noreply.github.com>
2023-01-15 22:34:22 +0000

    pesquisaAnuncios

--- anuncios.php
+++ anuncios.php
@@ -107,7 +107,7 @@ if(isset($_SESSION['nomeUser'])){ ?>
             <div class="col-3">
                   <div class="input-group">
                     <span class="input-group-text">Concelho</span>
-                    <select class="form-select" onchange="filtroConcelho(this.value)" id="listaConcelhos3">
+                    <select class="form-select" id="listaConcelhos3">
                     </select>
                   </div>
                 </div>
@@ -116,7 +116,7 @@ if(isset($_SESSION['nomeUser'])){ ?>
               <div class="col-3">
                   <div class="input-group">
                     <span class="input-group-text">Tipo de Negócio</span>
-                    <select class="form-select" onchange="filtroImovel(this.value)" id="tipoNegocImovel3">
+                    <select class="form-select" id="tipoNegocImovel3">
 
                     </select>
                   </div>
@@ -125,7 +125,7 @@ if(isset($_SESSION['nomeUser'])){ ?>
                 <div class="col-3">
                   <div class="input-group">
                     <span class="input-group-text">Tipo de Imóvel</span>
-                    <select class="form-select" onchange="filtroTipologia(this.value)" id="tipoImovel3">
+                    <select class="form-select"  id="tipoImovel3">
                     
                     </select>
                   </div>
@@ -193,22 +193,6 @@ if(isset($_SESSION['nomeUser'])){ ?>
     
                   </div>
     
-         
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-        
-
            
 
 
@@ -228,7 +212,8 @@ if(isset($_SESSION['nomeUser'])){ ?>
               <div class="col-md-2 ">
 
                 <div class="aa-single-advance-search">
-                  <input class="aa-search-btn" type="button" value="Procurar">
+                  <!-- <button class="aa-search-btn" type="button" value="Procurar" > -->
+                  <button class="aa-search-btn" onclick="pesquisaranuncios()" type="button" value="Procurar">Pesquisar</button>
                 </div>
 
               </div>
@@ -372,7 +357,7 @@ if(isset($_SESSION['nomeUser'])){ ?>
 
   
   <script src="assets/js/scriptPreco.js"></script> 
-
+  <script src="assets/js/pesquisa.js"></script> 
 
 
 
---
+++ assets/js/pesquisa.js
@@ -0,0 +1,74 @@
+function pesquisaranuncios(){
+  
+    let dados = new FormData();
+  
+    let concelho = $('#listaConcelhos3').val();
+    let tipneg = $('#tipoNegocImovel3').val();
+    let tipimovel = $('#tipoImovel3').val();
+    let tipologia = $('#tipologiaVenda3').val();
+    let precomin = $('#min-value').val();
+    let precomax = $('#max-value').val();
+
+
+    dados.append("concelho",concelho);
+    dados.append("tipneg",tipneg);
+    dados.append("tipimovel",tipimovel);
+    dados.append("tipologia",tipologia);
+    dados.append("precomin",precomin);
+    dados.append("precomax",precomax);
+  
+    dados.append("op",1);
+  
+    $.ajax({
+        url: "assets/model/modelpesquisa.php",
+        method: "POST",
+        data: dados,
+        dataType: "html",
+        cache:false,
+        contentType:false,
+        processData:false
+      })
+       
+      .done(function( resposta ) {
+        sucesso(resposta);
+
+      })
+       
+      .fail(function( jqXHR, textStatus ) {
+        alert( "Request failed: " + textStatus );
+      });
+  
+    
+  }
+
+ 
+
+      function sucesso(msg){
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: msg,
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+      
+
+      function error(msg){
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: msg,
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+
+
+
+$(function() {
+
+    
+  
+
+  });
